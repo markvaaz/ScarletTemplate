e86f20d731be1cfef7bfe6a3f6a26912d7129987
Mark Vaaz <marcosrodolfo32@gmail.com>
2026-01-02 16:55:17 -0300

    remove VampireCommandFramework dependency; update package version to 1.2.0 and content exclusion in package.csproj

--- Plugin.cs
+++ Plugin.cs
@@ -1,12 +1,12 @@
-﻿using BepInEx;
+﻿using System.Reflection;
+using BepInEx;
 using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
+using ScarletCore.Commanding;
 using ScarletCore.Data;
+using ScarletCore.Events;
 using ScarletCore.Systems;
-#if use_vcf
-using VampireCommandFramework;
-#endif
 #if use_rcon_soft
 using ScarletRCON.Shared;
 #endif
@@ -18,17 +18,13 @@ namespace ScarletTemplate;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 [BepInDependency("markvaaz.ScarletCore")]
-#if use_vcf
-[BepInDependency("gg.deca.VampireCommandFramework")]
-#endif
 #if use_rcon_soft
 [BepInDependency("markvaaz.ScarletRCON")] 
 #endif
 #if use_rcon_hard
 [BepInDependency("markvaaz.ScarletRCON")]
 #endif
-public class Plugin : BasePlugin
-{
+public class Plugin : BasePlugin {
   static Harmony _harmony;
   public static Harmony Harmony => _harmony;
   public static Plugin Instance { get; private set; }
@@ -36,22 +32,18 @@ public class Plugin : BasePlugin
   public static Settings Settings { get; private set; }
   public static Database Database { get; private set; }
 
-  public override void Load()
-  {
+  public override void Load() {
     Instance = this;
     LogInstance = Log;
 
     Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} version {MyPluginInfo.PLUGIN_VERSION} is loaded!");
 
     _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
-    _harmony.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+    _harmony.PatchAll(Assembly.GetExecutingAssembly());
 
     Settings = new Settings(MyPluginInfo.PLUGIN_GUID, Instance);
     Database = new Database(MyPluginInfo.PLUGIN_GUID);
     GameSystems.OnInitialize(Initialize);
-#if use_vcf
-    CommandRegistry.RegisterAll();
-#endif
 #if use_rcon_soft
     RconCommandRegistrar.RegisterAll();
 #endif
@@ -59,18 +51,13 @@ public class Plugin : BasePlugin
     CommandHandler.RegisterAll();
 #endif
   }
-  
-  private void Initialize()
-  {
-   
+
+  private void Initialize() {
+
   }
 
-  public override bool Unload()
-  {
+  public override bool Unload() {
     _harmony?.UnpatchSelf();
-#if use_vcf
-    CommandRegistry.UnregisterAssembly();
-#endif
 #if use_rcon_soft
     RconCommandRegistrar.UnregisterAssembly();
 #endif
@@ -82,20 +69,23 @@ public class Plugin : BasePlugin
     return true;
   }
 
-#if use_vcf
-/*
-  [CommandGroup("groupname")]
-  public class CommandGroup
-  {
-    [Command("commandname", "Description of the command")]
-    public static void CommandName(ChatCommandContext context)
-    {
-      // Command implementation
-      context.Reply("Command executed successfully!");
+  // Example command group using the new ScarletCore command system
+  [CommandGroup("example", Language.English, adminOnly: false)]
+  [CommandGroupAlias("exemplo", Language.Portuguese)]
+  public static class ExampleCommands {
+
+    [Command("hello", Language.English, description: "Say hello")]
+    [CommandAlias("ola", Language.Portuguese, description: "Dizer olá")]
+    public static void HelloCommand(CommandContext ctx) {
+      ctx.ReplySuccess($"Hello {ctx.User.CharacterName}!");
+    }
+
+    [Command("info", Language.English, description: "Get player info")]
+    [CommandAlias("informacao", Language.Portuguese, description: "Obter informações do jogador")]
+    public static void InfoCommand(CommandContext ctx, PlayerData player) {
+      ctx.ReplySuccess($"Player: {player.Name}, Level: {player.Level}");
     }
   }
-*/
-#endif
 
 #if use_rcon_soft || use_rcon_hard
 /*
